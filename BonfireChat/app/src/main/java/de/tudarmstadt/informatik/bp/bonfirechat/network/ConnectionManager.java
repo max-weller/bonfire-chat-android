e21ec8f93627b1ede5e05357f4001621e967c313
Simon-Konstantin Thiem <simonth@posteo.eu>
2015-08-12 18:08:13 +0200

    Listener in das Protocol geschrieben.

--- BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/network/ConnectionManager.java
+++ BonfireChat/app/src/main/java/de/tudarmstadt/informatik/bp/bonfirechat/network/ConnectionManager.java
@@ -138,6 +138,7 @@ public class ConnectionManager extends NonStopIntentService {
                 e.printStackTrace();
             }
             connections.add(p);
+            Log.d(TAG, "Die aktivierten connections sind: " + connections.toString());
         }
         return p;
     }
